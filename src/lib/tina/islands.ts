e0ac2f4db131c18533b7e8e9b54db1aa112e7a3a
Akarsh Bandi <akarshbandi82@gmail.com>
2026-09-22 21:27:29 +0530

    Add catch-all page route + restore global islands for live header/footer, remove orphan site folder

--- src/lib/tina/islands.ts
+++ src/lib/tina/islands.ts
@@ -1,10 +1,12 @@
 import type { IslandRegistry } from '@tinacms/astro/experimental';
 import type { QueryResult } from '@tinacms/astro/data';
-import type { PageQuery, JournalQuery } from '../../../tina/__generated__/types';
-import type { CmsPage, CmsJournal } from './data';
+import type { PageQuery, JournalQuery, ConfigQuery } from '../../../tina/__generated__/types';
+import type { CmsPage, CmsJournal, CmsConfig } from './data';
 import PageBody from '../../components/islands/PageBody.astro';
 import BlogBody from '../../components/islands/BlogBody.astro';
-import { getPage, getJournal } from './data';
+import Header from '../../components/Header.astro';
+import Footer from '../../components/Footer.astro';
+import { getPage, getJournal, getConfig } from './data';
 
 export const islands: IslandRegistry = {
   page: {
@@ -21,6 +23,22 @@ export const islands: IslandRegistry = {
     wrapper: { tag: 'article' },
     propsFromData: (data) => ({
       data: (data as QueryResult<JournalQuery>).data?.journal as CmsJournal | undefined,
+    }),
+  },
+  global: {
+    fetch: () => getConfig(),
+    component: Header,
+    wrapper: { tag: 'div' },
+    propsFromData: (data) => ({
+      config: (data as QueryResult<ConfigQuery>).data?.config as CmsConfig | undefined,
+    }),
+  },
+  'global-footer': {
+    fetch: () => getConfig(),
+    component: Footer,
+    wrapper: { tag: 'div' },
+    propsFromData: (data) => ({
+      config: (data as QueryResult<ConfigQuery>).data?.config as CmsConfig | undefined,
     }),
   },
 };
