08ea211f10674cdfa420b3a77f7d4b7d716fc71b
Akarsh Bandi <akarshbandi82@gmail.com>
2026-09-22 21:16:29 +0530

    Simplify live preview: single page/journal island, static header/footer per Astro Tina guide

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types.js";
-export const client = createClient({ url: "http://localhost:4001/graphql", token: "5873e3efd6d6c03a20d77fe17257388b1003983b", queries,  });
+export const client = createClient({ cacheDir: "/home/akarsh/ember-salt-astro/tina/__generated__/.cache/1790091959592", url: "http://localhost:4001/graphql", token: "5873e3efd6d6c03a20d77fe17257388b1003983b", queries,  });
 export default client;
   
